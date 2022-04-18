a3650191866b79c475e70f1e264dd3b49f31cfb5
devsajid <74415469+devsajid@users.noreply.github.com>
2022-04-18 18:26:15 +0600

    service section complete

--- src/Componenet/Pages/Home/Home/Home.js
+++ src/Componenet/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Services from "../Services/Services";
+import Studios from "../Studios/Studios";
 
 const Home = () => {
   return (
     <div>
       <h1>This is home</h1>
       <Services></Services>
+      <Studios></Studios>
     </div>
   );
 };
